008d73e2bba8c4d59b19c37f281186060977dbea
yuizhixiaocaiji <1390683299@qq.com>
2023-02-16 15:07:45 +0800

    新建node服务

---
+++ app.js
@@ -0,0 +1,41 @@
+// 引入express和mysql模块
+const express = require("express");
+const mysql = require("mysql");
+
+// 创建一个express实例
+const app = express();
+
+// 使用mysql模块连接数据库
+const connection = mysql.createConnection({
+  host: "localhost", // 数据库的地址
+  user: "root", // 数据库的用户名
+  password: "343527", // 数据库的密码
+  port: "3306", // 数据库的端口
+  database: "demo-1", // 数据库的名字
+});
+
+// 连接数据库
+connection.connect();
+
+// 定义一个get接口，接收请求参数，执行数据库操作，返回响应数据
+app.get("/api/users", (req, res) => {
+  // 获取请求参数
+  const name = req.query.name;
+  // 定义一个sql语句
+  const sql = `select * from people`;
+  // 执行sql语句
+  connection.query(spl, (err, result) => {
+    if (err) {
+      // 如果有错误，返回错误信息
+      res.send(err.message);
+    } else {
+      // 如果没有错误，返回查询结果
+      res.send(result);
+    }
+  });
+});
+
+// 启动服务器，监听3000端口
+app.listen(3000, () => {
+  console.log("Server is running at http://localhost:3000");
+});
